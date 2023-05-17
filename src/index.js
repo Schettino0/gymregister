2bbd6b852518312c6ae5ddf0fe4ce73fd6af338c
Eduardo Schettino <100874849+Schettino0@users.noreply.github.com>
2023-05-17 19:15:01 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = require("./app");
+const port = process.env.PORT || 8080;
 
-const httpServer = app.listen(443,'10.113.118.25' , () => {
+const httpServer = app.listen(port , () => {
     console.log(`Server Running at port 3000`);
 })
 
